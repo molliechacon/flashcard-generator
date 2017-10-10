b327f1669932ef1d84efd4667b1dfdbfc4ec4f5d
mollie chacon <mollie.mosley@gmail.com>
2017-10-10 17:27:26 -0600

    most functionality intact; though has promises issues

--- CLI.js
+++ CLI.js
@@ -15,15 +15,132 @@ inquirer.prompt([
         ]
     }
 ]).then(function(answer) {
-    var myNewBasic = new BasicCard();
-    var myNewCloze = new ClozeCard()
-    if (answer.command === "Add a basic flashcard?") {
-        myNewBasic.addBasic();
+
+    if (answer.command === "Add a basic flashcard?") {     
+        addBasic();
+        
     } else if (answer.command === "Add a cloze-deleted flashcard?") {
-        myNewCloze.addCloze();
+        addCloze();
     }
 });
 
 
 
+// would love to list the functions below as prototypes of the constructors contained within basiccard.js and clozecard.js but can't get the promises/asynchronous problem fixed... At least they kind of work when placed here...
 
+function addBasic() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "front",
+            message: "Please list your question for the front of the card:",
+            validate: function(input) {
+                if (input === "") {
+                    console.log("Please provide a question.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }, {
+            type: "input",
+            name: "back",
+            message: "Please provide the answer.",
+            validate: function(input) {
+                if (input === "") {
+                    console.log("Please list your answer.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then(function(answers) {
+
+        // THIS CONSOLE.LOG WORKS!
+        console.log(answers);
+
+        // BUT BASICCARD IS NOT DEFINED BELOW!!!!!! WHY???????? BASICCARD.JS IS REQUIRED ABOVE!!!!!!!!! THE CONSOLE.LOG W/IN THE BASICCARD.JS FILE EXECUTES SO I BELIEVE IT IS REQUIRED PROPERLY??
+        
+        // PROMISES?!?! WTF? HELP.
+
+        // here is the error message from the CL: (node:11788) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 1): ReferenceError: BasicCard is not defined
+        var newBasic = new BasicCard(answers.front, answers.back);
+
+        console.log(newBasic.front);
+        logBasic();
+    });     
+};
+
+
+function logBasic() {
+    var basicData = "/nType: Basic Card" + "/nQuestion: " + newBasic.front + "/nAnswer: " + newBasic.back;
+    
+    fs.appendFile("log.txt", basicData, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Your flashcard has been added.");
+    });
+};
+
+
+function addCloze() {
+    inquirer.prompt([
+        {
+            type: "input",
+             name: "text",
+            message: "Please list the full text for the front of the card:",
+            validate: function(input) {
+                if (input === "") {
+                    console.log("Please provide your full text statement.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }, {
+            type: "input",
+            name: "cloze",
+            message: "Please retype the cloze-deletion portion of your statement for the back of the card.",
+            validate: function(input) {
+                if (input === "") {
+                    console.log("Please provide the cloze-deletion.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    ]).then(function(answers) {
+
+        // this console.log works
+        console.log(answers);
+
+
+        // same issue here as above. ClozeCard is not defined... Why?
+        var newCloze = new ClozeCard(answers.text, answers.cloze)
+        logCloze();
+    }); 
+};
+
+
+function logCloze() {
+    var clozeData = "/nType: Cloze-Deleted Card" + "/nFront(partial text): " + newCloze.partial + "/nCloze(back): " + newCloze.back;
+    
+    fs.appendFile("log.txt", clozeData, function(err) {
+        if (err) {
+            console.log(err)
+        }
+        console.log("Your flashcard has been added.");
+    });
+        
+     console.log(clozeData);
+};
+
+
+// consider adding a prompt for viewing flashcards that have been created. 
+
+
+
+    
